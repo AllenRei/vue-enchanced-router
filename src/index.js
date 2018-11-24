1217b42bd504fe755b38b9d0c2b81e93ee6c018e
Oleksandr <email@e.com>
2018-11-24 13:45:06 +0200

    route mapper changes

--- src/index.js
+++ src/index.js
@@ -11,23 +11,42 @@ import {
 
 export const RouteGuard = RouteGuard;
 
-const routeMapper = (spinnerComponent, errorComponent) => route => ({
-    ...route,
-    beforeEnter: Array.isArray(route.beforeEnter) ?
-        RouteGuard(route.beforeEnter) :
-        route.beforeEnter,
-    component: route.loaders ?
-        LoaderFor(
-            route.component, 
-            route.loaders, 
-            spinnerComponent, 
-            errorComponent
-        ) :
-        route.component,
-    children: route.children ?
-        route.children.map(routeMapper) :
-        route.children
-});
+function routeMapper(spinnerComponent, errorComponent) {
+    return (route) => {
+        if (Array.isArray(route.beforeEnter)) {
+            newRoute.beforeEnter = RouteGuard(route.beforeEnter);
+        }
+        if (route.loaders) {
+            newRoute.component = LoaderFor(
+                route.component,
+                route.loaders,
+                spinnerComponent,
+                errorComponent);
+        }
+        if (route.children) {
+            newRoute.children = route.children.map(routeMapper);
+        }
+        return route;
+    }
+}
+
+// const routeMapper = => (route) => ({
+//     ...route,
+//     beforeEnter: Array.isArray(route.beforeEnter) ?
+//         RouteGuard(route.beforeEnter) :
+//         route.beforeEnter,
+//     component: route.loaders ?
+//         LoaderFor(
+//             route.component, 
+//             route.loaders, 
+//             spinnerComponent, 
+//             errorComponent
+//         ) :
+//         route.component,
+//     children: route.children ?
+//         route.children.map(routeMapper) :
+//         route.children
+// });
 
 export class EnchancedRouter extends Router {
     constructor(data) {
